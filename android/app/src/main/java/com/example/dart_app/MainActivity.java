06823dc8ee8f75289fe3189728dba275559804ce
Admin <quanghuymang@gmail.com>
2023-03-13 20:53:37 +0700

    this 18

---
+++ android/app/src/main/java/com/example/dart_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.dart_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.lightBlue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.ac_unit),
+          onPressed: (){
+            print('leading');
+          },
+        ),
+        title: Text('The first flutter app'),
+      ),
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+
+          ],
+        ),
+      )
+    );
+  }
+}
+
+
